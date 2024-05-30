c40f6cd860885af21b8acb8aede69949cdc3d3e2
s111ew <167631312+s111ew@users.noreply.github.com>
2024-05-30 16:07:50 +0100

    Add solution using for loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
 
+const sumAll = function(num1, num2) {
+    let finalNum = 0
+    for (let i = num1; i <= num2; i++) {
+        finalNum += i;
+    }
+    return finalNum;
 };
 
 // Do not edit below this line
