6aba4ae378f97f754b4ffdb9b5a97929071194d6
s111ew <167631312+s111ew@users.noreply.github.com>
2024-05-30 16:45:25 +0100

    Add solution using && comparison operator - all tests passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+//LEAP YEARS
+//------------
+//IF DIVISIBLE BY 4 == TRUE
+//BUT IF DIVISIBLE BY 100 == FALSE
+//UNLESS IF DIVISIBLE BY 400 == TRUE
 
+
+const leapYears = function(num) {
+    if ((num % 100 == 0) && (num % 400 != 0)) {
+        return false;
+    } else if (num % 4 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
