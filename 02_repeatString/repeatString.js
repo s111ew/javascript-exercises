d693ad07b20b70c12e73891312345ecb66c1b963
s111ew <167631312+s111ew@users.noreply.github.com>
2024-05-30 12:52:40 +0100

    Added solution to excercise 02 - all tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+   let text = string;
+   let i = 0;
+   if (num < 0) {
+    return "ERROR"
+   } else {
+   while (i <= num) {
+    text = string.repeat(i)
+    i++;
+   }
+}
+   return text;
 };
 
 // Do not edit below this line
