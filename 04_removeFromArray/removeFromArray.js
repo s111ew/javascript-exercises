b71accdab034f36fe26d8d68ba8d549e268a1c37
s111ew <167631312+s111ew@users.noreply.github.com>
2024-05-30 14:19:37 +0100

    Add solution using 'filter' array method - all tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
-};
+// Implement a function that takes an array and some other arguments then removes the other arguments from that array:
+
+// removeFromArray([1, 2, 3, 4], 3);
+
+//REMOVE ARGUMENTS FROM ARRAY
+//---------------------------
+//RECEIVE AN ARRAY AS AN ARGUMENT
+//RECEIVE N AMOUNT OF OTHER NUMERIC ARGUMENTS
+//CHECK ARRAY TO SEE IF ANY OF THE ARGUMENTS ARE PRESENT
+//IF THEY ARE PRESENT, REMOVE THEM
+//RETURN NEW ARRAY
+
+
+
+
+
+
+const removeFromArray = function(array, ...args) {
+    return array.filter(item => !args.includes(item));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
