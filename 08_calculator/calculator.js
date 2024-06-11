f702cef91f37f0c128e3aa396154cc383bc15515
s111ew <167631312+s111ew@users.noreply.github.com>
2024-06-11 17:40:11 +0100

    Add complete code, all tests passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	 return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length < 1) {
+    return 0
+  } else{
+	return arr.reduce((total, current) => total + current);
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, current) => total * current);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorialArray = []
+	if (num === 0) {
+    return 1;
+  }
+  for (let i = 1; i <= num; i++) {
+    factorialArray[i] = i;
+  }
+  return factorialArray.reduce((total, current) => total * current)
 };
 
 // Do not edit below this line
