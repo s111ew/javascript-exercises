d22100935cb417193ec43790a80e50a75d7b16e3
s111ew <167631312+s111ew@users.noreply.github.com>
2024-06-12 11:43:11 +0100

    Add solution, all tests passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    string = string.replace(/ /g,"")
 
+    string = string.toLowerCase();
+    const stringArray = string.split("");
+
+    if (stringArray.length % 2 !== 0) {
+    stringArray.splice( (Math.floor(stringArray.length / 2)), 1)
+    };
+
+    const stringArrayStart = JSON.stringify(stringArray.slice(0, (stringArray.length / 2)));
+    const stringArrayEnd = JSON.stringify(stringArray.slice((stringArray.length / 2)).reverse())
+
+    if (stringArrayStart === stringArrayEnd) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
---
+++ 09_palindromes/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+if (stringArray.length / 2 !== 0) {
+    stringArray.splice( (Math.floor(stringArray.length / 2)), 1)
+    }
