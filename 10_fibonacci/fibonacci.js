4b2769137e11f648dc05b03f0e0a7aa0c1bf3e37
s111ew <167631312+s111ew@users.noreply.github.com>
2024-06-12 12:06:58 +0100

    Add solution, all tests passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = parseInt(num)
+    let sequence = [0, 1];
+    if (num < 0) {
+        return 'OOPS'
+    } else if (num === 0) {
+        return 0
+    } else {
+        for (let i = 2; i <= num; i++) {
+            sequence[i] = sequence[i - 2] + sequence[i - 1]
+        }
+    return sequence[num]
+    }
 };
+
+console.log(fibonacci(8))
 
 // Do not edit below this line
 module.exports = fibonacci;
