52edcb8ff57812fa65534b7782458c283990fccc
s111ew <167631312+s111ew@users.noreply.github.com>
2024-06-12 12:55:28 +0100

    Add solution, all tests passed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+  const findTheOldest = function(arr) {
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
 
+    for (let i = 0; i < arr.length; i++) {
+        if (!arr[i].yearOfDeath) {
+            arr[i].yearOfDeath = currentYear;
+        }
+    }
+
+    const sortedArray = arr.sort((a, b) => {
+        const ageA = a.yearOfDeath - a.yearOfBirth;
+        const ageB = b.yearOfDeath - b.yearOfBirth;
+        return ageB - ageA;
+    });
+    return sortedArray[0];
 };
 
 // Do not edit below this line
